76e8e6322345a5346743345115c752a53e2f3d62
TimnaSoroka <timna.soroka@weizmann.ac.il>
2025-06-27 11:29:35 +0300

    Delete fingerprint_code directory

--- fingerprint_code/cleanmatrix/row_norm.m
+++
@@ -1,8 +0,0 @@
-function norms_vec = row_norm(mat)
-%ROW_NORM Summary of this function goes here
-%   Detailed explanation goes here
-% Slightly faster than vec_norm based on 100 runs of 100*1000
-
-    norms_vec = sqrt(sum(mat.^2, 2));
-end
-
